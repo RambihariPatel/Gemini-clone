5fe8ddb4a10f9c21e8ddc701f7895464dc52ab62
Rambihari Patel <rambiharipatel175@gmail.com>
2026-01-12 12:09:28 +0530

    main

---
+++ src/components/Main/Main.jsx
@@ -0,0 +1,52 @@
+import React from 'react'
+import './Main.css'
+import { assets } from '../../assets/assets'
+
+const Main = () => {
+  return (
+    <div className='main'>
+      <div className='nav'>
+        <p>Gemini</p>
+        <img src={assets.user_icon} alt=''/> 
+      </div>
+
+      <div className="main-container">
+         <div className="grid">
+            <p>
+                <span>
+                    Hello, Ram.
+                </span>
+            </p>
+            <p>
+                How can I help you Today ?
+            </p>
+         </div>
+         <div className="cards">
+
+            <div className="card">
+                <p>Suggest beautiful places to see on an upcoming road trip</p>
+                <img src = {assets.compass_icon} alt='' />
+            </div>
+
+            <div className="card">
+                <p>Brifly summarize this concept: urban planning</p>
+                <img src = {assets.bulb_icon} alt='' />
+            </div>
+
+            <div className="card">
+                <p>Brainstorm team bonding activities for our work retreat</p>
+                <img src = {assets.message_icon} alt='' />
+            </div>
+
+            <div className="card">
+                <p>Improve the readability of the following code</p>
+                <img src = {assets.code_icon} alt='' />
+            </div>
+
+         </div>
+      </div>
+    </div>
+  )
+}
+
+export default Main
